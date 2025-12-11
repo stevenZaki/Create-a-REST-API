fd1cf8b51d9191346d4b46e96f0c0f3551624136
unknown <stevensivo27@gmail.com>
2025-12-11 22:59:37 +0200

    set up database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable({})
 export class AuthService {
-  login() {}
-  signup() {}
+  signup() {
+    return { msg: 'I have signed up' };
+  }
+  signin() {
+    return { msg: 'I have signed in' };
+  }
 }
